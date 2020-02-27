c52b97ce40251524c204184c3fb12a9b7ba0e882
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-02-27 17:24:24 -0600

    exported user model

---
+++ models/Users.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+//NOTES TO SELF TO ENHANCE UNDERSTANDING
+//We already created a cluster in activity #2  with MongoDB
+//We also created a .gitignore file to avoid unneccessary file transfers
+//We then switched to a new branch and ran npm init which created a package.json file
+//We installed various dependencies which were downloaded to our node_modules folder, 
+//they were also added to dependency object in our package.json file.
+
+//Create a schema for users, similar to creating a series of tables in SQL
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+});
+
+//Generate a model named 'User' mongoose.model('name',schema)
+//Models in mongoose are fancy constructors compiled from schema definitions
+const User = mongoose.model('user',UserSchema);
+
+//Export the User model to make it accessible to the rest of your application
+//export is a javascript keyword I need to research further...
+export default User;
