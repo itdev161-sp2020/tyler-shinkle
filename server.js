91e95498ce59a448c51a1eacd7d72954dbf304f7
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-02-27 19:21:42 -0600

    added validation

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import connectDatabase from './config/db';
+//importing check and validationResult which are named exports (hence the curly braces)
+//no braces will import whatever the default export is...
+import {check,validationResult} from 'express-validator';
 
 //initialize express application
 const app = express();
 
-//connect database
+//connect database // function within db.js file we imported above.
 connectDatabase();
 
 //configure middleware
@@ -26,12 +29,35 @@ app.get('/',(req,res)=>
  */
 
  //When a post is made to our 'api/users' page log the request body and respond with it.
- app.post('/api/users',(req,res)=>{
-     console.log(req.body);
-     res.send(req.body);
- });
+ app.post(
+     //destination
+     '/api/users',
+     //functions which return an error if true.
+     [
+        check('name','Please enter your name.')
+            .not()
+            .isEmpty(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters.'
+        ).isLength({min:6})
+     ],
+     //function with request and response objects
+     (req,res)=>{
+     //errors caught from imported express-validator and above code
+     const errors = validationResult(req);
+     //if there is an error return error object array
+     if(!errors.isEmpty()){
+         return res.status(442).json({errors:errors.array() });
+     //otherwise return request body
+     } else{
+         return res.send(req.body);
+     }
+    }
+ );
 
 //connection listener
 app.listen(3000, ()=>console.log('Express server running on port 3000'));
 
 //to test run npm run server and navigate to localhost:3000
+//postman helps us test this api and build custom headers / bodies
