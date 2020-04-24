cb55d35ddfbf5d0105479bcdfa50768245944442
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-04-23 19:35:55 -0500

    api endpoint to get posts started

--- server.js
+++ server.js
@@ -215,6 +215,17 @@ app.post(
     }
 );
 
+//GET POSTS
+app.get('/api/posts',auth,async(req,res)=>{
+    try{
+        const posts = await Post.find().sort({date:-1});
+        res.json(posts);
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
  const returnToken = (user,res) => {
      const payload ={
          user:{
