74de7a02d9c01414f77883673a0c4ac5c1fdce83
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-04-23 21:17:50 -0500

    Completed Activity 9

--- server.js
+++ server.js
@@ -262,7 +262,33 @@ app.delete('/api/posts/:id',auth,async(req,res)=>{
         console.error(error);
         res.status(500).send('Server error.');
     }
-})
+});
+
+//UPDATE A POST
+app.put('/api/posts/:id',auth,async(req,res)=>{
+    try{
+        const {title,body} = req.body;
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({msg:'Post not found.'});
+        }
+
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg:'User not authorized.'});
+        }
+
+        post.title = title || post.title;
+        post.body = body || post.body;
+
+        await post.save();
+
+        res.json(post);
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
 
  const returnToken = (user,res) => {
      const payload ={
